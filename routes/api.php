305d58597c62aa3ba50b4ded5b0cd3906e4a81d7
bengeos <george.beng@gmail.com>
2019-10-05 14:43:58 +0300

    update chnages

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,27 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group(['namespace' => 'Authenticate'], function () {
+    Route::post('/authenticate', 'AuthController@authenticate');
+    Route::post('/register', 'AuthController@register');
+    Route::post('/reset_code', 'AuthController@getResetCode');
+    Route::post('/reset_password', 'AuthController@resetPassword');
+});
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['namespace' => 'Users'], function () {
+    Route::get('/user_types', 'UsersController@getUserTypes');
+    Route::get('/users', 'UsersController@getUsers');
+    Route::get('/users_paginated', 'UsersController@getPaginatedUsers');
+    Route::post('/user', 'UsersController@create');
+    Route::post('/user_status', 'UsersController@changeUserStatus');
+    Route::patch('/user', 'UsersController@update');
+    Route::delete('/user/{id}', 'UsersController@delete');
+});
+
+Route::group(['namespace' => 'MessagePorts'], function () {
+    Route::get('/message_ports', 'MessagePortsController@getMessagePortList');
+    Route::get('/message_ports_paginated', 'MessagePortsController@getMessagePortsPaginated');
+    Route::post('/message_port', 'MessagePortsController@create');
+    Route::patch('/message_port', 'MessagePortsController@update');
+    Route::delete('/message_port/{id}', 'MessagePortsController@delete');
 });
