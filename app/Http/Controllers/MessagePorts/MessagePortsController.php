411215eec2560c34569fb341383eda284885da1a
bengeos <george.beng@gmail.com>
2019-10-05 14:36:04 +0300

    update chnages

---
+++ app/Http/Controllers/MessagePorts/MessagePortsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\MessagePorts;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MessagePortsController extends Controller
+{
+
+    /**
+     * MessagePortsController constructor.
+     */
+    public function __construct()
+    {
+
+    }
+}
